c16b1919bec40fb76e1907b5e82035e07dcf40e0
Lukas Hodel <s0545881@htw-berlin.de>
2015-01-31 11:13:00 +0100

    Removes unused files in root

--- build_gravitron.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-qmake /Users/richard/repos/gravitron_game/Gravitron/Gravitron.pro;
-make /Users/richard/repos/gravitron_game/Gravitron/Makefile;
-open -a /Users/richard/repos/gravitron_game/build/Gravitron.app;
